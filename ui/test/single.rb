e907c6357daa9a7e779b58b0c01e2adaeb654724
l3kn <hello@l3kn.de>
2013-06-30 16:48:51 +0200

    fixed test function

--- ui/test/single.rb
+++ ui/test/single.rb
@@ -1,12 +1,10 @@
-#!/usr/bin/ruby
- 
 require 'uri'
 require 'net/http'
 
-#$url = 'http://www.area42.l3kn.de'
-$url = 'http://localhost:42337'
+$url = 'http://www.satar.l3kn.de'
+#$url = 'http://localhost:42337'
 
-def post1(node,event,id)
+def post(node,event,id)
 	ts = (Time.now.to_f*1000).floor
 	puts ts
 	params = {'NODE' => node,'TSN' => (ts-1000.0) ,'EVENT' => event,'ID' => id}
@@ -21,14 +19,6 @@ def post2(node,event,id)
 end
 
 
-#post1(1,101,111) #init
-post2(2,0,111)
-post2(2,1,111)
-post2(2,101,111)
-post2(2,101,111)
-#for j in 1..20 do
-#for i in 1..5 do
-#	post1(i,101,0) #init
-#	sleep(0.01)
-#end
-#end
+post(2,0,111)
+post(2,1,111)
+10.times { post(2,101,111) }
