7de2780c77af45af7466939cecbe0b6eaa1d7241
Alex Shure <shure@etemu.com>
2013-08-31 11:15:49 +0200

    outsourced 'Graphics'. added mysql structure

---
+++ server/satar.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 31, 2013 at 11:12 AM
+-- Server version: 5.5.31
+-- PHP Version: 5.4.6-1ubuntu1.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `satar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satar`
+--
+
+CREATE TABLE IF NOT EXISTS `satar` (
+  `index` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `TSS` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'TimeStamp of the local Server',
+  `TSN` bigint(20) unsigned NOT NULL COMMENT 'TimeStamp of the Node in ms since last reset',
+  `EVENT` int(10) unsigned NOT NULL COMMENT 'Type of event',
+  `ID` int(10) unsigned NOT NULL COMMENT 'ID, e.g. unique number of the competition''s participant',
+  `NODE` int(10) unsigned NOT NULL COMMENT 'SATAR Node ID, unique number of the sending SATAR controller',
+  PRIMARY KEY (`index`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='contains raw values from the SATAR nodes' AUTO_INCREMENT=45908 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satar_results`
+--
+
+CREATE TABLE IF NOT EXISTS `satar_results` (
+  `runID` int(11) NOT NULL AUTO_INCREMENT,
+  `raceID` int(11) NOT NULL,
+  `riderID` int(11) NOT NULL,
+  `runTime` int(11) NOT NULL,
+  `onTrack` tinyint(1) NOT NULL,
+  PRIMARY KEY (`runID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satar_users`
+--
+
+CREATE TABLE IF NOT EXISTS `satar_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) unsigned NOT NULL DEFAULT '0',
+  `firstname` varchar(254) NOT NULL,
+  `lastname` varchar(254) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `team` varchar(254) NOT NULL,
+  `disclaimer` varchar(254) NOT NULL,
+  `registered` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `ip` int(11) NOT NULL,
+  `size` varchar(254) NOT NULL,
+  `city` varchar(254) NOT NULL,
+  `street` varchar(254) NOT NULL,
+  `streetno` int(11) unsigned NOT NULL,
+  `zipcode` varchar(254) NOT NULL,
+  `dob` date NOT NULL DEFAULT '0000-00-00',
+  `sex` varchar(254) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table for all registered participants. ' AUTO_INCREMENT=728 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
