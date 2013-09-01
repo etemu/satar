b6b5b1a0e1d268afab5896c34d8f4350e84228e5
Alex Shure <shure@etemu.com>
2013-09-02 01:54:28 +0200

    outsourced license information to LICENSE file, add standard config, remove TestData dir, cosmetics
    plus workaround for older nodes, added SatarServer dir back into the
    repo for easier deployment with old nodes.

---
+++ SatarServer/config/config.example.php
@@ -0,0 +1,21 @@
+<?php
+//  etemu.com, Alex Shure CC-BY-SA-NC 3.0
+//      ____      ____    _____    ____      ____ CC 
+//  ___(_ (_`____/ () \__|_   _|__/ () \____| () )_____   
+//    .__)__)   /__/\__\   |_|   /__/\__\   |_|\_\
+//  System for Advanced Timekeeping and Amateur Racing.
+//
+// This is the configuration for the MySQL database connection.  
+// Change the values below to your MySQL server data.
+// The MySQL table name is hardcoded and will be "satar".
+//
+$sql_host		= 'localhost'; 		// the database server, e.g. localhost
+$sql_user 		= 'USER'; 			// username who with read+write access
+$sql_pass 		= 'PASSWORD'; 		// the password
+$sql_db 		= 'satar'; 			// the database which is used
+
+$ruby_APIhost = 'localhost'; 		// Host adress of the Ruby Server.
+$ruby_APIport = '42337'; 			// Port of the Ruby Server.
+$ruby_APIpath = '/api/event' 		// Path to the SatarServerRuby's API
+
+?>
---
+++ SatarServer/satar.php
@@ -0,0 +1,135 @@
+<?php
+//  etemu.com, Alex Shure CC-BY-SA 3.0
+//      ____      ____    _____    ____      ____ CC 
+//  ___(_ (_`____/ () \__|_   _|__/ () \____| () )_____   
+//    .__)__)   /__/\__\   |_|   /__/\__\   |_|\_\
+//  System for Advanced Timekeeping and Amateur Racing.
+//
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  !!   Do not change any code in this file.  		  !!
+//  !! All configuration is made in config/config.php !!
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+include 'config/config.php';
+include 'config/satardb.php';
+
+// store the values of the incoming GET request -> MySQL database
+
+$sql="INSERT INTO satar (TSN,EVENT,ID,NODE)
+VALUES
+('$_REQUEST[T]','$_REQUEST[E]','$_REQUEST[I]','$_REQUEST[N]')";
+
+if (!mysql_query($sql,$con))
+  {
+  die('ERR: DB ' . mysql_error());
+  }
+
+echo "ETH: SQL ACK.\n";
+mysql_close($con);
+
+// Submit this array to the Ruby API for further processing
+
+$post_data = array(
+    'TSN' => $_REQUEST['T'],
+    'EVENT' => $_REQUEST['E'],
+	'ID' => $_REQUEST['I'],
+    'NODE' => $_REQUEST['N']
+);
+ 
+// Send a POST request to the server API 
+$result = post_request($ruby_APIhost, $ruby_APIpath, $ruby_APIport, $post_data);
+ 
+if ($result['status'] == 'ok'){
+ 
+	if ($result['content'])
+		echo "WRN: !SSR API ERR\n";
+		
+	if ($result['content']=='')
+		echo "ETH: SSR ACK.\n";
+	
+	// Print headers 
+    // echo $result['header']; 
+ 
+    // echo '<hr />';
+ 
+    // print the result of the whole request:
+    // echo $result['content'];
+	
+ 
+}
+else {
+    echo 'ERR: SSR API: ' . $result['error']; 
+}
+
+
+function post_request($host, $path, $port, $data, $referer='SatarServer') {
+ 
+    // Convert the data array into URL Parameters like a=b&foo=bar etc.
+    $data = http_build_query($data);
+ 
+    // parse the given URL (aShure: deprecated)
+    /* $url = parse_url($url);
+ 
+    if ($url['scheme'] != 'http') { 
+        die('ERR: Not a http scheme.');
+    }
+	*/
+	
+	/*
+    // debug host and path:
+    //$host = $ruby_APIhost;
+	echo 'DEB: ruby_APIhost: ' . $host . '<br>';
+    //$path = $ruby_APIpath;
+	echo 'DEB: ruby_APIpath: ' . $path . '<br>';
+	//$port = $ruby_APIport;
+	echo 'DEB: ruby_APIport: ' . $port . '<br>';
+	*/
+	
+    // open a socket connection on port $ruby_APIport - timeout: 8 sec
+    $fp = fsockopen($host, $port, $errno, $errstr, 8);
+ 
+    if ($fp){
+ 
+        // send the request headers:
+        fputs($fp, "POST $path HTTP/1.1\r\n");
+        fputs($fp, "Host: $host\r\n");
+ 
+        if ($referer != '')
+            fputs($fp, "Referer: $referer\r\n");
+ 
+        fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
+        fputs($fp, "Content-length: ". strlen($data) ."\r\n");
+        fputs($fp, "Connection: close\r\n\r\n");
+        fputs($fp, $data);
+ 
+        $result = ''; 
+        while(!feof($fp)) {
+            // receive the results of the request
+            $result .= fgets($fp, 128);
+        }
+    }
+    else { 
+        return array(
+            'status' => 'err', 
+            'error' => "$errstr ($errno)"
+        );
+    }
+ 
+    // close the socket connection:
+    fclose($fp);
+ 
+    // split the result header from the content
+    $result = explode("\r\n\r\n", $result, 2);
+ 
+    $header = isset($result[0]) ? $result[0] : '';
+    $content = isset($result[1]) ? $result[1] : '';
+ 
+    // return as structured array:
+    return array(
+        'status' => 'ok',
+        'header' => $header,
+        'content' => $content
+    );
+}
+
+?>
